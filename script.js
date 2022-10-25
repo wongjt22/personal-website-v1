39d0c21df17921ac6736a72e6e2f706f718ff16e
Jackson Wong <wongjt@berkeley.edu>
2022-10-24 17:23:30 -0700

    navbar fade (buggy) and  about text

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const fadeIn = () => {
+const contentFade = () => {
   var reveals = document.querySelectorAll('.reveal');
 
   for (var i = 0; i < reveals.length; i++) {
@@ -15,5 +15,21 @@ const fadeIn = () => {
   }
 }
 
-window.addEventListener('scroll', fadeIn);
+const navbarFade = () => {
+  var navbar = document.querySelector('.navbar');
+  var scroll = 0;
+  var newScroll = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+
+  if (newScroll > scroll) {
+    navbar.classList.remove('visible');
+  } else {
+    navbar.classList.add('visible');
+  }
+  scroll = newScroll <= 0 ? 0 : newScroll;
+}
+
+
+window.addEventListener('scroll', contentFade);
+window.addEventListener('scroll', navbarFade);
+
 
